e08afa156b645078d481dd382f341c6a1e0d0c70
Shahabaddin Sarkheil <31788480+saarkheil@users.noreply.github.com>
2023-06-16 18:55:46 +0200

    Config WebPack

---
+++ e2e.mjs
@@ -0,0 +1,11 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({headless:"new"});
+  const page = await browser.newPage();
+  await page.goto('http://localhost:3000/');
+  await page.setViewport({width: 1080, height: 1024});
+  await page.waitForSelector(".App-header");
+  console.log("OK");
+  await browser.close();
+})();
