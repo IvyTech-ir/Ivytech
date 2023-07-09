7575f20c524d32f2eb224891c0d7b10763f2b1d8
Shahabaddin Sarkheil <31788480+saarkheil@users.noreply.github.com>
2023-07-09 13:41:32 +0200

    add react modules

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,48 @@ import { Counter } from './components';
 import './assets/css/App.css';
 import './assets/css/sb-admin-2.css';
 import './assets/js/sb-admin-2';
+import MasterLayout from "../src/views/masterLayout/index";
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <Counter />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <span>
-                    <span>Learn </span>
-                    <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-                        React
-                    </a>
-                    <span>, </span>
-                    <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-                        Redux
-                    </a>
-                    <span>, </span>
-                    <a
-                        className="App-link"
-                        href="https://redux-toolkit.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Redux Toolkit
-                    </a>
-                    ,<span> and </span>
-                    <a
-                        className="App-link"
-                        href="https://react-redux.js.org/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        React Redux
-                    </a>
-                </span>
-            </header>
-        </div>
+        <MasterLayout>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <Counter />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <span>
+                        <span>Learn </span>
+                        <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+                            React
+                        </a>
+                        <span>, </span>
+                        <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
+                            Redux
+                        </a>
+                        <span>, </span>
+                        <a
+                            className="App-link"
+                            href="https://redux-toolkit.js.org/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Redux Toolkit
+                        </a>
+                        ,<span> and </span>
+                        <a
+                            className="App-link"
+                            href="https://react-redux.js.org/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            React Redux
+                        </a>
+                    </span>
+                </header>
+            </div>
+        </MasterLayout>
     );
 }
 
