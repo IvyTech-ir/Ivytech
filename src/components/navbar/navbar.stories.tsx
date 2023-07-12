989a95362f7af8823cf3615fc3b610d4c99b8c1d
Shahabaddin Sarkheil <31788480+saarkheil@users.noreply.github.com>
2023-07-12 19:54:49 +0200

    Define Sample Components

---
+++ src/components/navbar/navbar.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './navbar';
+import ThemeProvider from './../../views/masterLayout/providers/themeProvider';
+import type { Meta, ReactRenderer, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Navbar> = {
+  component: Navbar,
+  decorators: [
+    (Story) => (
+      <ThemeProvider  >
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export default meta;
+type CounterStory = StoryObj<typeof Navbar>;
+export const Default: CounterStory = {};
---
+++ src/views/masterLayout/providers/themeProvider.tsx
@@ -0,0 +1,6 @@
+import './../../../assets/css/App.css';
+import './../../../assets/css/sb-admin-2.css';
+import './../../../../public/assets/vendor/fontawesome-free/css/all.css';
+export default function ThemeProvider(props: any) {
+    return props.children;
+}
